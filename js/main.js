d33a956e1c242b5c6d9ae4c6a82483f740595d9e
RodrigoHerrera <rohesino@gmail.com>
2023-04-28 19:38:40 -0300

    preentrega js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
-let edad = prompt("ingrese su edad");
+function saludar() {
+  let nombre = prompt("ingrese su nombre");
+  let edad = prompt("ingrese su edad");
 
-if (edad > 17) {
-  alert("Edad aceptable");
-} else {
-  alert("Edad Rechazada");
+  if (edad > 17) {
+    let mensaje = `Bienvenido ${nombre} a Nidal`;
+    alert(mensaje);
+    console.log(`Bienvenido a la Consola ${nombre}. Proceda con cuidado.`);
+  } else {
+    let mensaje1 = `${nombre} no tiene permitido estar aqui. Porfavor salga de aqui.`;
+    alert(mensaje1);
+  }
 }
+saludar();
+
+function nidal() {
+  let trabajo = prompt(
+    `Ingrese el trabajo deseado "escolta , caceria , busqueda , reconocimiento".`
+  );
+  switch (trabajo) {
+    case `escolta`:
+      alert(`Su pedido de ${trabajo} a sido aceptado.`);
+      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
+      break;
+    case `caceria`:
+      alert(`Su pedido de ${trabajo} a sido aceptado.`);
+      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
+      break;
+    case `busqueda`:
+      alert(`Su pedido de ${trabajo} a sido aceptado.`);
+      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
+      break;
+    case `reconocimiento`:
+      alert(`Su pedido de ${trabajo} a sido aceptado.`);
+      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
+      break;
+
+    default:
+      alert("Escoja un trabajo designado.");
+      console.log(`Su pedido de ${trabajo} no es posible.`);
+      break;
+  }
+}
+nidal();
