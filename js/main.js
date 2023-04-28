5dc12a6a30405aca674ab4d539d823f7dd78c77f
RodrigoHerrera <rohesino@gmail.com>
2023-04-28 20:16:07 -0300

    preentrega js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function saludar() {
     let mensaje = `Bienvenido ${nombre} a Nidal`;
     alert(mensaje);
     console.log(`Bienvenido a la Consola ${nombre}. Proceda con cuidado.`);
+    nidal();
   } else {
     let mensaje1 = `${nombre} no tiene permitido estar aqui. Porfavor salga de aqui.`;
     alert(mensaje1);
@@ -14,9 +15,17 @@ function saludar() {
 saludar();
 
 function nidal() {
-  let trabajo = prompt(
-    `Ingrese el trabajo deseado "escolta , caceria , busqueda , reconocimiento".`
-  );
+  let trabajo = null;
+  while (
+    trabajo != "escolta" &&
+    trabajo != "caceria" &&
+    trabajo != "busqueda" &&
+    trabajo != "reconocimiento"
+  ) {
+    trabajo = prompt(
+      `Ingrese el trabajo deseado "escolta , caceria , busqueda , reconocimiento".`
+    );
+  }
   switch (trabajo) {
     case `escolta`:
       alert(`Su pedido de ${trabajo} a sido aceptado.`);
@@ -34,11 +43,5 @@ function nidal() {
       alert(`Su pedido de ${trabajo} a sido aceptado.`);
       console.log(`Su pedido de ${trabajo} a sido autorizado.`);
       break;
-
-    default:
-      alert("Escoja un trabajo designado.");
-      console.log(`Su pedido de ${trabajo} no es posible.`);
-      break;
   }
 }
-nidal();
