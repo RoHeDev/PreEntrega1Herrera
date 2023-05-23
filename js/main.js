7e84ef9b5900491569235e8f7f8175a06a36e713
RodrigoHerrera <rohesino@gmail.com>
2023-05-23 16:37:06 -0300

    segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+const preciosTrabajos = {
+  escolta: 20000,
+  caceria: 45000,
+  busqueda: 15000,
+  reconocimiento: 10500,
+};
+
+const trabajosRealizados = []; // contador de trabajos
+
 function saludar() {
-  let nombre = prompt("ingrese su nombre");
-  let edad = prompt("ingrese su edad");
+  let nombre = prompt("Ingrese su nombre");
+  let edad = parseInt(prompt("Ingrese su edad"));
 
   if (edad > 17) {
     let mensaje = `Bienvenido ${nombre} a Nidal`;
@@ -8,40 +17,54 @@ function saludar() {
     console.log(`Bienvenido a la Consola ${nombre}. Proceda con cuidado.`);
     nidal();
   } else {
-    let mensaje1 = `${nombre} no tiene permitido estar aqui. Porfavor salga de aqui.`;
+    let mensaje1 = `${nombre} no tiene permitido estar aquí. Por favor salga de aquí.`;
     alert(mensaje1);
   }
 }
-saludar();
 
 function nidal() {
   let trabajo = null;
   while (
-    trabajo != "escolta" &&
-    trabajo != "caceria" &&
-    trabajo != "busqueda" &&
-    trabajo != "reconocimiento"
+    trabajo !== "escolta" &&
+    trabajo !== "caceria" &&
+    trabajo !== "busqueda" &&
+    trabajo !== "reconocimiento"
   ) {
     trabajo = prompt(
-      `Ingrese el trabajo deseado "escolta , caceria , busqueda , reconocimiento".`
+      `Ingrese el trabajo deseado: "escolta", "caceria", "busqueda", "reconocimiento".`
     );
   }
-  switch (trabajo) {
-    case `escolta`:
-      alert(`Su pedido de ${trabajo} a sido aceptado.`);
-      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
-      break;
-    case `caceria`:
-      alert(`Su pedido de ${trabajo} a sido aceptado.`);
-      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
-      break;
-    case `busqueda`:
-      alert(`Su pedido de ${trabajo} a sido aceptado.`);
-      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
-      break;
-    case `reconocimiento`:
-      alert(`Su pedido de ${trabajo} a sido aceptado.`);
-      console.log(`Su pedido de ${trabajo} a sido autorizado.`);
-      break;
+
+  let precioTrabajo = preciosTrabajos[trabajo];
+
+  if (trabajo && precioTrabajo) {
+    let codigoDescuento = prompt("Ingrese el código de descuento (opcional)");
+
+    if (codigoDescuento === "fantoche") {
+      precioTrabajo -= precioTrabajo * 0.15; // descuentito del 15%
+    }
+
+    let confirmacion = confirm(
+      `El precio del trabajo "${trabajo}" es: $${precioTrabajo}. ¿Desea confirmar la contratación?`
+    );
+
+    if (confirmacion) {
+      alert(`¡El trabajo "${trabajo}" ha sido contratado!`);
+      console.log(
+        `El trabajo "${trabajo}" ha sido contratado por ${precioTrabajo}`
+      );
+
+      trabajosRealizados.push({
+        trabajo: trabajo,
+        precio: precioTrabajo,
+      });
+    } else {
+      alert("La contratación ha sido cancelada.");
+    }
+  } else {
+    console.log("Trabajo inválido");
   }
 }
+
+saludar();
+console.log("Trabajos realizados:", trabajosRealizados);
