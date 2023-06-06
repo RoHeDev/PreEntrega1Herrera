8e18964eb6d9d954906e8990a70b099dc2872965
RodrigoHerrera <rohesino@gmail.com>
2023-06-06 18:57:57 -0300

    tercerpreentrega

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const preciosTrabajos = {
   reconocimiento: 10500,
 };
 
-const trabajosRealizados = []; // contador de trabajos
+let trabajosRealizados = [];
 
 function saludar() {
   let nombre = prompt("Ingrese su nombre");
@@ -15,26 +15,85 @@ function saludar() {
     let mensaje = `Bienvenido ${nombre} a Nidal`;
     alert(mensaje);
     console.log(`Bienvenido a la Consola ${nombre}. Proceda con cuidado.`);
-    nidal();
+    document.getElementById("loginButton").style.display = "none"; // Oculta el botón de login
+
+    // Evento boton 0
+    document.getElementById("contratar1").addEventListener("click", () => {
+      nidal("escolta");
+    });
+
+    // Evento boton 1
+    document.getElementById("contratar2").addEventListener("click", () => {
+      nidal("caceria");
+    });
+
+    // Evento boton 2
+    document.getElementById("contratar3").addEventListener("click", () => {
+      nidal("busqueda");
+    });
+
+    // Evento boton 3
+    document.getElementById("contratar4").addEventListener("click", () => {
+      nidal("reconocimiento");
+    });
+
+    // Cargar los trabajos realizados solo si coinciden con los datos de inicio de sesión
+    cargarTrabajosRealizados(nombre, edad);
   } else {
     let mensaje1 = `${nombre} no tiene permitido estar aquí. Por favor salga de aquí.`;
     alert(mensaje1);
   }
 }
 
-function nidal() {
-  let trabajo = null;
-  while (
-    trabajo !== "escolta" &&
-    trabajo !== "caceria" &&
-    trabajo !== "busqueda" &&
-    trabajo !== "reconocimiento"
-  ) {
-    trabajo = prompt(
-      `Ingrese el trabajo deseado: "escolta", "caceria", "busqueda", "reconocimiento".`
-    );
-  }
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", saludar);
 
+console.log("Trabajos realizados:", trabajosRealizados);
+
+const trabajos = document.querySelectorAll(".trabajo");
+
+trabajos[0].innerText = "ESCOLTA";
+trabajos[1].innerText = "CACERIA";
+trabajos[2].innerText = "BUSQUEDA";
+trabajos[3].innerText = "RECONOCIMIENTO";
+
+function mostrarTrabajosRealizados() {
+  const trabajosRealizadosContainer =
+    document.getElementById("trabajosRealizados");
+
+  trabajosRealizadosContainer.innerHTML = ""; // Limpia el contenido anterior
+
+  trabajosRealizados.forEach((trabajo) => {
+    const trabajoRealizado = document.createElement("p");
+    trabajoRealizado.textContent = `Trabajo: ${trabajo.trabajo}, Precio: $${trabajo.precio}`;
+    trabajosRealizadosContainer.appendChild(trabajoRealizado);
+  });
+}
+
+// Función para guardar los trabajos realizados en el almacenamiento local
+function guardarTrabajosRealizados() {
+  localStorage.setItem(
+    "trabajosRealizados",
+    JSON.stringify(trabajosRealizados)
+  );
+}
+
+// Función para cargar los trabajos realizados desde el almacenamiento local
+function cargarTrabajosRealizados(nombre, edad) {
+  const trabajosGuardados = localStorage.getItem("trabajosRealizados");
+  if (trabajosGuardados) {
+    trabajosRealizados = JSON.parse(trabajosGuardados);
+
+    // Filtrar los trabajos realizados para mostrar solo los que coinciden con los datos de inicio de sesión
+    trabajosRealizados = trabajosRealizados.filter((trabajo) => {
+      return trabajo.nombre === nombre && trabajo.edad === edad;
+    });
+
+    mostrarTrabajosRealizados();
+  }
+}
+
+function nidal(trabajo) {
   let precioTrabajo = preciosTrabajos[trabajo];
 
   if (trabajo && precioTrabajo) {
@@ -54,10 +113,18 @@ function nidal() {
         `El trabajo "${trabajo}" ha sido contratado por ${precioTrabajo}`
       );
 
+      // Agregar los datos de inicio de sesión junto con el trabajo realizado
       trabajosRealizados.push({
+        nombre: nombre,
+        edad: edad,
         trabajo: trabajo,
         precio: precioTrabajo,
       });
+
+      mostrarTrabajosRealizados();
+
+      // Guardar los trabajos realizados en el almacenamiento local
+      guardarTrabajosRealizados();
     } else {
       alert("La contratación ha sido cancelada.");
     }
@@ -66,5 +133,5 @@ function nidal() {
   }
 }
 
-saludar();
-console.log("Trabajos realizados:", trabajosRealizados);
+// Al cargar la página, llamar a la función para cargar los trabajos realizados desde el almacenamiento local
+cargarTrabajosRealizados();
